f75ef1c1a4ef43bdf16cd16cbb88dc1b9a768046
wangchen7152 <18335185814@163.com>
2022-10-03 21:54:15 +0800

    机构课程开发

--- gsonline/wang9172/apps/organization/urls.py
+++ gsonline/wang9172/apps/organization/urls.py
@@ -2,11 +2,12 @@
 
 __author__ = 'wang'
 
-from django.conf.urls import url, include
-from .views import AskUser, OrgList, OrgHome
+from django.conf.urls import url
+from .views import AskUser, OrgList, OrgHome, CourseHome
 
 urlpatterns = [
     url(r'^user_ask/$', AskUser.as_view(), name="user_ask"),
     url(r'^list/$', OrgList.as_view(), name='list'),
     url(r'^home/(?P<org_id>\d+)$', OrgHome.as_view(), name='org_home'),
+    url(r'^course/(?P<org_id>\d+)$', CourseHome.as_view(), name='org_course'),
 ]
