8ee7adb6f58eac66bbf8ca9f877c8ebc78a6bad4
wangchen7152 <18335185814@163.com>
2022-10-16 20:07:51 +0800

    over gxonline

--- gsonline/wang9172/apps/courses/adminx.py
+++ gsonline/wang9172/apps/courses/adminx.py
@@ -5,6 +5,7 @@ __author__ = 'wang'
 import xadmin
 
 from .models import Course, Lesson, Video, CourseResource, BannerCourse
+from courses.models import CourseOrg
 
 
 class LessonInline(object):
@@ -13,20 +14,46 @@ class LessonInline(object):
 
 
 class CourseAdmin(object):
+    # 列表展示
     list_display = ['name', 'desc', 'detail', 'degree', 'learn_time',
-                    'student_num', 'fav_num', 'image']
+                    'student_num', 'fav_num', 'image', 'get_lesson_nums',
+                    'go_to']
+    # 可搜索内容
     search_fields = ['name', 'desc', 'detail', 'degree', 'learn_time',
                      'student_num', 'fav_num', 'image']
+
+    # 列表过滤
     list_filter = ['name', 'desc', 'detail', 'degree', 'learn_time',
                    'student_num', 'fav_num', 'image']
+    # 默认排序
     ordering = ['-click_num']
+    # 设置只读字段
     readonly_fields = ['click_num']
+    # 设置编辑页面不展示编辑列
+    exclude = ['fav_num']
+    # 一个model两个模型
     inlines = [LessonInline]
+    # 设置列表页直接编辑内容
+    list_editable = ['degree', 'desc']
+    # 设置页面刷新,秒为单位
+    refresh_times = [3, 10]
+    # 富文本编辑器
+    style_fields = {"detail": "ueditor"}
 
     def queryset(self):
         ba = super(CourseAdmin, self).queryset()
         ba = ba.filter(is_banner=False)
         return ba
+
+    def save_model(self):
+        # 保存课程统计机构课程数
+        obj = self.new_obj
+        obj.save()
+        if obj.course_org is not None:
+            course_org = obj.couse_org
+            course_org.course_nums = CourseOrg.objects.filter(
+                course_org=course_org).count()
+            course_org.save()
 
 
 class BannerAdmin(object):
@@ -39,6 +66,7 @@ class BannerAdmin(object):
     ordering = ['-click_num']
     readonly_fields = ['click_num']
     inlines = [LessonInline]
+    list_editable = ['degree', 'name']
 
     def queryset(self):
         ba = super(BannerAdmin, self).queryset()
